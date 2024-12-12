67c04312c05c2974eb8c3fec471e2efbcc849d3f
Manoj <khanavalkarmanoj@gmail.com>
2024-12-12 09:54:16 +0530

    views and car application created

--- carSellingWeb/cars/views.py
+++ carSellingWeb/cars/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
-
+from .models import cars
+from .forms import CarForm
+from django.shortcuts import get_object_or_404,redirect
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def car_list(request):
+    cars_list= cars.objects.all().order_by('-created_at')
+    return render(request,'cars_list.html',{'cars':cars})
+
+def car_create(request):
+    if request.method == "POST":
+        form=CarForm(request.POST , request.FILES)
+        if form.is_valid():
+            car=form.save(commit=False)
+            car.user=request.user
+            car.save()
+            return redirect('car_list')
+    else:
+        form=CarForm()
+    return render(request,'car_form.html',{'form':form})
+
+def car_edit(request,car_id):
+    car=get_object_or_404(cars,pk=car_id,user=request.user)
+    if request.method == 'POST':
+        form= CarForm(request.POST, request.FILES,instance=car)
+        if form.is_valid():
+            car=form.save(commit=False)
+            car.user =request.user
+            car.save()
+            return redirect('car_list')
+    else:
+        form= CarForm(instance=car)
+    return render(request,'car_form.html',{'form':form})
+
+def car_delete(request,car_id):
+    car= get_object_or_404(cars,pk=car_id,user= request.user)
+    if request.method == "POST":
+        car.delete()
+        return redirect('car_list')
+    return render(request,'car_confirm_delete.html',{'car':car})
